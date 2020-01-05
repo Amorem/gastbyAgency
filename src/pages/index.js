d5ac00170528f9c9f13509c27fece8abe7765e70
Amorem <dev@amorem.com>
2020-01-05 20:57:24 +0100

    Section One completed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,23 @@
 import React from "react"
+import { Link } from "gatsby"
+import { Banner, TextWrapper, MoreText } from "../styles/IndexStyles"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div style={{ postion: "relative" }}>
+    <Banner></Banner>
+    <TextWrapper>
+      <div>
+        <h2>GeekyHacker</h2>
+        <p>
+          One Stop For
+          <br />
+          All your development
+          <br />
+          And design needs
+        </p>
+        <Link to="/works">Our Work</Link>
+      </div>
+    </TextWrapper>
+    <MoreText>Learn More</MoreText>
+  </div>
+)
