d4986d49f45380f75b295c3d4f99eec29dc4e2c0
Amorem <dev@amorem.com>
2020-01-07 12:38:16 +0100

    Change primary/accent colors

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,30 +8,38 @@ import {
   MoreText,
   SectionTwo,
   SectionThree,
+  SectionFour,
   FlexBoxIndex,
 } from "../styles/IndexStyles"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { library } from "@fortawesome/fontawesome-svg-core"
 import { faHeart, faCode, faGem, fas } from "@fortawesome/free-solid-svg-icons"
+import {
+  fab,
+  faHtml5,
+  faJs,
+  faReact,
+  faCss3,
+  faGalacticSenate,
+} from "@fortawesome/free-brands-svg-icons"
 
 library.add(faHeart, faCode, faGem, fas)
 
 export default () => (
   <>
-    <section style={{ postion: "relative" }}>
+    <section>
       <Banner></Banner>
       <TextWrapper>
         <div>
           <h2>BS Move</h2>
           <GenereicPara>
-            Offrez-vous un déménagement à la hauteur de vos attentes !
+            Un déménagement à la hauteur de vos attentes
           </GenereicPara>
-          <Link to="/works">Votre devis en 1 click</Link>
+          <Link to="/works">Votre devis en 1 clic</Link>
         </div>
       </TextWrapper>
-      <MoreText>Learn More</MoreText>
     </section>
-    <SectionTwo>
+    {/* <SectionTwo>
       <div>
         <h2>Our Passion</h2>
         <GenereicPara>
@@ -65,7 +73,7 @@ export default () => (
           border
         />
       </span>
-    </SectionTwo>
+    </SectionTwo> */}
     <SectionThree>
       <FlexBoxIndex>
         <div className="image">
@@ -98,5 +106,110 @@ export default () => (
         </div>
       </FlexBoxIndex>
     </SectionThree>
+    <SectionFour>
+      <div className="header__section4">
+        <div className="title__section4">L'EXPERTISE BS MOVE</div>
+        <GenereicPara lessSize>
+          Basée à Meaux, en Ile de France, notre équipe vous accompagnera pas à
+          pas et restera à votre écoute tout au long de votre déménagement.
+          <br />
+          <br /> Nos déménageurs BS MOVE sont régulièrement formés afin de
+          toujours vous offrir le meilleur service possible.
+          <br />
+          <br />
+          Nous nous occupons de l’ensemble de vos biens, et nous les déplaçons
+          vers votre futur logement.
+          <br />
+          <br /> Finis le stress, vous ne pensez plus à rien ! Nous transportons
+          vos biens en toute sécurité vers votre nouveau logement et les plaçons
+          où vous le souhaitez.
+          <br />
+          <br />
+        </GenereicPara>
+      </div>
+      <div className="grid__section4">
+        <div className="item1" style={{ backgroundColor: "#1D508E" }}>
+          <div className="flex__section4">
+            <FontAwesomeIcon
+              icon={faReact}
+              color="#00FFCC"
+              size="3x"
+              fixedWidth
+            />
+            <GenericH2 none>React</GenericH2>
+          </div>
+          <GenereicPara lessSize lessSpacing grey>
+            Modern JavaScript framework which will make your web application
+            extremely fast and, at the same time, handy for every user.
+          </GenereicPara>
+        </div>
+        <div className="item2" style={{ backgroundColor: "#4A4D89" }}>
+          <div className="flex__section4">
+            <FontAwesomeIcon icon="code" color="#00FFCC" size="3x" fixedWidth />
+            <GenericH2 none>React Native</GenericH2>
+          </div>
+          <GenereicPara lessSize lessSpacing grey>
+            Cross-platform for mobile app development based on Javascript, whose
+            resulting code is compiled to Android and iOS.
+          </GenereicPara>
+        </div>
+        <div className="item3" style={{ backgroundColor: "#484A83" }}>
+          <div className="flex__section4">
+            <FontAwesomeIcon icon={faJs} color="#00FFCC" size="3x" fixedWidth />
+            <GenericH2 none>JavaScript</GenericH2>
+          </div>
+          <GenereicPara lessSize lessSpacing grey>
+            JavaScript is the language of the web. It is used for Web
+            development, mobile development and app development and everything
+            else.
+          </GenereicPara>
+        </div>
+        <div className="item4" style={{ backgroundColor: "#45477E" }}>
+          <div className="flex__section4">
+            <FontAwesomeIcon
+              icon={faHtml5}
+              color="#00FFCC"
+              size="3x"
+              fixedWidth
+            />
+            <GenericH2 none>HTML5</GenericH2>
+          </div>
+          <GenereicPara lessSize lessSpacing grey>
+            HTML, a standardized system for tagging text files to achieve font,
+            colour, graphic, and hyperlink effects on World Wide Web pages.
+          </GenereicPara>
+        </div>
+        <div className="item5" style={{ backgroundColor: "#424479" }}>
+          <div className="flex__section4">
+            <FontAwesomeIcon
+              icon={faCss3}
+              color="#00FFCC"
+              size="3x"
+              fixedWidth
+            />
+            <GenericH2 none>CSS3</GenericH2>
+          </div>
+          <GenereicPara lessSize lessSpacing grey>
+            CSS is a style sheet language used for describing the presentation
+            of a document written in a markup language like HTML.
+          </GenereicPara>
+        </div>
+        <div className="item6" style={{ backgroundColor: "#3F4174" }}>
+          <div className="flex__section4">
+            <FontAwesomeIcon
+              icon={faGalacticSenate}
+              color="#00FFCC"
+              size="3x"
+              fixedWidth
+            />
+            <GenericH2 none>Gatsby</GenericH2>
+          </div>
+          <GenereicPara lessSize lessSpacing grey>
+            Gatsby is a free and open source framework based on React that helps
+            developers build blazing fast websites and apps
+          </GenereicPara>
+        </div>
+      </div>
+    </SectionFour>
   </>
 )
--- src/styles/IndexStyles.js
+++ src/styles/IndexStyles.js
@@ -1,12 +1,12 @@
 import styled from "styled-components"
 
 const GenereicPara = styled.p`
-  text-transform: uppercase;
+  /* text-transform: uppercase; */
   text-align: center;
   letter-spacing: ${props => (props.lessSpacing ? "0.075em" : "0.225em")};
   font-size: ${props => (props.lessSize ? "1.5rem" : "2.5rem")};
   line-height: ${props => (props.lessSize ? "2rem" : "3rem")};
-  color: ${props => (props.grey ? "#c8ece9" : "#ffffff")};
+  color: ${props => (props ? props : "#ffffff")};
 `
 
 const GenericH2 = styled.h2`
@@ -19,13 +19,30 @@ const GenericH2 = styled.h2`
   margin: ${props => (props.some ? "5rem 0 0 0" : "0")};
 `
 
+const GenericH3 = styled.h3`
+  font-size: 3rem;
+  padding: ${props => (props.none ? "0" : "1.35em 0")};
+  color: ${props => (props.dark ? "#4E4852" : "#ffffff")};
+  border-bottom: ${props => (props.none ? "0" : "2px solid #1d9c91")};
+  text-transform: uppercase;
+  letter-spacing: 0.6rem;
+  margin: 0;
+`
+const GenereicParaAbout = styled.p`
+  text-transform: uppercase;
+  text-align: center;
+  letter-spacing: ${props => (props.lessSpacing ? "0.075em" : "0.225em")};
+  font-size: ${props => (props.lessSize ? "1.5rem" : "2.5rem")};
+  line-height: ${props => (props.lessSize ? "2rem" : "3rem")};
+  color: ${props => (props.grey ? "#4E4852" : "#ffffff")};
+`
+
 const Banner = styled.div`
   &:after {
     content: "";
     display: block;
     height: 50vh;
     width: 100%;
-
     background-image: linear-gradient(rgba(10, 92, 124, 0.2), black),
       url("banner.jpg");
     background-size: cover;
@@ -52,12 +69,13 @@ const TextWrapper = styled.div`
     font-size: 5rem;
     opacity: 1;
     padding: 0.35em 1em;
-    border-top: 2px solid white;
-    border-bottom: 2px solid white;
+    /* border-top: 2px solid white;
+    border-bottom: 2px solid white; */
     text-transform: uppercase;
     margin: 0;
   }
   a {
+    margin-top: 30px;
     background-color: #ed4933;
     box-shadow: none;
     color: #ffffff;
@@ -110,7 +128,7 @@ const SectionTwo = styled.section`
 `
 
 const SectionThree = styled.section`
-  background-color: #2b343d;
+  background-color: #1d508e;
   color: #ffffff;
 `
 
@@ -132,12 +150,52 @@ const FlexBoxIndex = styled.div`
   }
 `
 
+const SectionFour = styled.section`
+  background-color: #fff;
+  color: #1d508e;
+  text-align: center;
+  .header__section4 {
+    width: 66%;
+    margin: 0 auto;
+  }
+  .title__section4 {
+    font-size: 3rem;
+    padding: 1.35em 0;
+    color: #1d508e;
+    /* border-bottom: 2px solid #1d508e; */
+    text-transform: uppercase;
+    letter-spacing: 0.225em;
+    margin: 0;
+    margin-top: 20px;
+  }
+  .grid__section4 {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    width: 66%;
+    margin: 0 auto;
+    padding: 3rem 0;
+  }
+  .grid__section4 > * {
+    padding: 3rem;
+  }
+  .flex__section4 {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 1rem;
+  }
+  .flex__section4 > h2 {
+    margin-left: 1rem;
+  }
+`
+
 export {
   Banner,
   TextWrapper,
   MoreText,
   SectionTwo,
   SectionThree,
+  SectionFour,
   FlexBoxIndex,
   GenereicPara,
   GenericH2,
